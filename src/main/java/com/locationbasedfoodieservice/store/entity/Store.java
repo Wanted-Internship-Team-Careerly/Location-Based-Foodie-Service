3a4515d13137dc89806ffc2f09fced76c00736e4
9898s <sueh9898@icloud.com>
2023-10-31 23:54:49 +0900

    complete: Store 엔티티 설정

---
+++ src/main/java/com/locationbasedfoodieservice/store/entity/Store.java
@@ -0,0 +1,57 @@
+package com.locationbasedfoodieservice.store.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.locationbasedfoodieservice.review.entity.Review;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Store {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, unique = true)
+	private String nameAddress;
+
+	@Column
+	private String city;
+
+	@Column
+	private String name;
+
+	@Column
+	private String licenseDate;
+
+	@Column
+	private String businessStatus;
+
+	@Column
+	private String type;
+
+	@Column
+	private String streetAddress;
+
+	@Column
+	private String lotNumberAddress;
+
+	@Column
+	private String postalCode;
+
+	@Column
+	private Double longitude;
+
+	@Column
+	private Double latitude;
+
+	@OneToMany(mappedBy = "store", orphanRemoval = true)
+	private List<Review> reviewList = new ArrayList<>();
+}
