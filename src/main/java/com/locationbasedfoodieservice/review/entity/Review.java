c1dd674fb2ff8ee3dc77ff1e4ae8721ebe9ee0bc
9898s <sueh9898@icloud.com>
2023-10-31 23:54:07 +0900

    complete: Review 엔티티 설정

---
+++ src/main/java/com/locationbasedfoodieservice/review/entity/Review.java
@@ -0,0 +1,35 @@
+package com.locationbasedfoodieservice.review.entity;
+
+import com.locationbasedfoodieservice.member.entity.Member;
+import com.locationbasedfoodieservice.store.entity.Store;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Review {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private Integer score;
+
+	@Column(nullable = false)
+	private String content;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private Member member;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "store_id")
+	private Store store;
+}
