5329487c3b4217427ccec2e343acfe689fa98209
9898s <sueh9898@icloud.com>
2023-10-31 23:58:45 +0900

    complete: ApiResponseDto 설정

---
+++ src/main/java/com/locationbasedfoodieservice/common/dto/ApiResponseDto.java
@@ -0,0 +1,14 @@
+package com.locationbasedfoodieservice.common.dto;
+
+import lombok.Getter;
+
+@Getter
+public class ApiResponseDto {
+	private final int statusCode;
+	private final String statusMessage;
+
+	public ApiResponseDto(int statusCode, String statusMessage) {
+		this.statusCode = statusCode;
+		this.statusMessage = statusMessage;
+	}
+}
