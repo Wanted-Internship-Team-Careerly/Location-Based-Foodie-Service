9c02c871b84bf265f6ba13c99592b94d099c08b3
9898s <sueh9898@icloud.com>
2023-11-02 10:05:15 +0900

    refactor: 위도, 경도 validation 수정

--- src/main/java/com/locationbasedfoodieservice/member/dto/MemberSignupRequestDto.java
+++ src/main/java/com/locationbasedfoodieservice/member/dto/MemberSignupRequestDto.java
@@ -5,6 +5,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import com.locationbasedfoodieservice.member.entity.Member;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -18,8 +19,12 @@ public class MemberSignupRequestDto {
 	@NotBlank(message = "패스워드를 입력해주시길 바랍니다.")
 	private String password;
 
-	private double latitude;
-	private double longitude;
+	@NotNull(message = "위도 값을 입력해주시길 바랍니다.")
+	private Double latitude;
+
+	@NotNull(message = "경도 값을 입력해주시길 바랍니다.")
+	private Double longitude;
+
 	private boolean isSuggestion;
 
 	public Member toEntity(BCryptPasswordEncoder passwordEncoder) {
