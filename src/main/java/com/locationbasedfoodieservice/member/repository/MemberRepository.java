372014cbda2ed1f854feacdce871646d55bd7e04
9898s <sueh9898@icloud.com>
2023-11-02 21:45:02 +0900

    complete: member security 설정

--- src/main/java/com/locationbasedfoodieservice/member/repository/MemberRepository.java
+++ src/main/java/com/locationbasedfoodieservice/member/repository/MemberRepository.java
@@ -1,10 +1,14 @@
 package com.locationbasedfoodieservice.member.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.locationbasedfoodieservice.member.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+	Optional<Member> findByAccount(String account);
 
 	boolean existsByAccount(String account);
 }
