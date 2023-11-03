4cc8a58a8f1fd46df165ddc92642b7bbe78dd48f
9898s <sueh9898@icloud.com>
2023-11-04 02:09:01 +0900

    feat: 회원 설정 업데이트 API

--- src/main/java/com/locationbasedfoodieservice/member/controller/MemberController.java
+++ src/main/java/com/locationbasedfoodieservice/member/controller/MemberController.java
@@ -2,13 +2,18 @@ package com.locationbasedfoodieservice.member.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.locationbasedfoodieservice.common.config.auth.LoginMember;
 import com.locationbasedfoodieservice.common.dto.ApiResponseDto;
 import com.locationbasedfoodieservice.member.dto.MemberSignupRequestDto;
+import com.locationbasedfoodieservice.member.dto.MemberUpdateRequestDto;
 import com.locationbasedfoodieservice.member.service.MemberService;
 
 import jakarta.validation.Valid;
@@ -26,5 +31,15 @@ public class MemberController {
 		memberService.signup(requestDto);
 
 		return ResponseEntity.ok().body(new ApiResponseDto(HttpStatus.CREATED.value(), "회원 가입 완료"));
+	}
+
+	@PutMapping("/{memberId}")
+	public ResponseEntity<ApiResponseDto> update(
+		@PathVariable Long memberId,
+		@RequestBody @Valid MemberUpdateRequestDto requestDto,
+		@AuthenticationPrincipal LoginMember loginMember) {
+		memberService.update(memberId, requestDto, loginMember);
+
+		return ResponseEntity.ok(new ApiResponseDto(HttpStatus.OK.value(), "회원 정보 업데이트"));
 	}
 }
