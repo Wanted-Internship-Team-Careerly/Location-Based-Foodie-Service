f7fd7a211b8bc68c44b6539f72e3f360f2233117
suhwan <sueh9898@icloud.com>
2023-11-04 15:14:45 +0900

    [feature] 회원 설정 업데이트 API (#19)
    * feat: 회원 설정 업데이트 API
    
    * test: 업데이트 성공 테스트
    
    * test: 찾을 수 없는 회원 번호일 경우 업데이트 실패
    
    * test: 토큰 계정이 일치하지 않을 경우 업데이트 실패
    
    * refactor: ErrorCode 추상적인 내용으로 변경
    
    * refactor: 회원 정보 업데이트 @Valid 제거

--- src/main/java/com/locationbasedfoodieservice/common/error/CustomErrorCode.java
+++ src/main/java/com/locationbasedfoodieservice/common/error/CustomErrorCode.java
@@ -11,7 +11,8 @@ public enum CustomErrorCode {
 
 	// 예시 에러코드입니다.
 	USER_NOT_FOUND(HttpStatus.BAD_REQUEST.value(), "존재하지 않는 사용자입니다."),
-	USER_ALREADY_EXIST(HttpStatus.BAD_REQUEST.value(), "이미 존재하는 계정입니다.");
+	USER_ALREADY_EXIST(HttpStatus.BAD_REQUEST.value(), "이미 존재하는 계정입니다."),
+	TOKEN_USER_MISMATCH(HttpStatus.BAD_REQUEST.value(), "계정에 잘못된 접근입니다.");
 
 	private final int errorCode;
 	private final String errorMessage;
