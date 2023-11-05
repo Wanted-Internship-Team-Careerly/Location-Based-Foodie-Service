d9295a3b22cfa87c92f2052c7374aa21b52f5eba
jonghun <jonghuncu@gmail.com>
2023-11-06 05:37:23 +0900

    feat : LoginMember 추가

--- src/main/java/com/locationbasedfoodieservice/review/controller/ReviewController.java
+++ src/main/java/com/locationbasedfoodieservice/review/controller/ReviewController.java
@@ -1,5 +1,6 @@
 package com.locationbasedfoodieservice.review.controller;
 
+import com.locationbasedfoodieservice.common.config.auth.LoginMember;
 import com.locationbasedfoodieservice.review.dto.request.ReviewRequestDto;
 import com.locationbasedfoodieservice.review.dto.response.ReviewResponseDto;
 import com.locationbasedfoodieservice.review.service.ReviewService;
@@ -7,6 +8,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,9 +22,10 @@ public class ReviewController {
     private final ReviewService reviewService;
 
     @PostMapping
-    public ResponseEntity<ReviewResponseDto> createReview(@RequestBody @Valid ReviewRequestDto request) {
+    public ResponseEntity<ReviewResponseDto> createReview(@RequestBody @Valid ReviewRequestDto request,
+            @AuthenticationPrincipal LoginMember loginMember) {
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(reviewService.createReview(request));
+        return ResponseEntity.status(HttpStatus.CREATED).body(reviewService.createReview(request, loginMember));
     }
 
 }
