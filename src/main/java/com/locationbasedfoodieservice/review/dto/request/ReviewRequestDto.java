1a26ca57455622110a653cb3dbafcb820540f30d
jonghun <jonghuncu@gmail.com>
2023-11-06 05:39:24 +0900

    fix : dto param 참조타입으로 변경

--- src/main/java/com/locationbasedfoodieservice/review/dto/request/ReviewRequestDto.java
+++ src/main/java/com/locationbasedfoodieservice/review/dto/request/ReviewRequestDto.java
@@ -3,6 +3,9 @@ package com.locationbasedfoodieservice.review.dto.request;
 import com.locationbasedfoodieservice.member.entity.Member;
 import com.locationbasedfoodieservice.restaurant.entity.Restaurant;
 import com.locationbasedfoodieservice.review.entity.Review;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -10,8 +13,10 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public class ReviewRequestDto {
-    @NotNull
-    private int score;
+    @NotNull(message = "평점은 공백일 수 없습니다")
+    @Min(1)
+    @Max(5)
+    private Integer score;
 
     private String content;
 
