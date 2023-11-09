07c238f5d30a57e5de2acd44c4d7e225eb72912a
jonghun <jonghuncu@gmail.com>
2023-11-10 08:35:01 +0900

    feat : dto property 변경

--- src/main/java/com/locationbasedfoodieservice/restaurant/dto/response/RestaurantResponseDto.java
+++ src/main/java/com/locationbasedfoodieservice/restaurant/dto/response/RestaurantResponseDto.java
@@ -1,6 +1,7 @@
 package com.locationbasedfoodieservice.restaurant.dto.response;
 
 import com.locationbasedfoodieservice.restaurant.entity.Restaurant;
+import com.locationbasedfoodieservice.review.dto.response.ReviewResponseDto;
 import com.locationbasedfoodieservice.review.entity.Review;
 import java.util.List;
 import lombok.AllArgsConstructor;
@@ -12,12 +13,36 @@ import lombok.Getter;
 @Builder
 public class RestaurantResponseDto {
 
-    private Restaurant restaurant;
-    private List<Review> reviewList;
+    private Long restaurantId;
+    private String nameAddress;
+    private String city;
+    private String name;
+    private String licenseDate;
+    private String businessStatus;
+    private String type;
+    private String streetAddress;
+    private String lotNumberAddress;
+    private String postalCode;
+    private double longitude;
+    private double latitude;
+    private double rating;
+    private List<ReviewResponseDto> reviewList;
 
-    public static RestaurantResponseDto from(Restaurant restaurant, List<Review> reviewList) {
+    public static RestaurantResponseDto from(Restaurant restaurant, List<ReviewResponseDto> reviewList) {
         return RestaurantResponseDto.builder()
-                .restaurant(restaurant)
+                .restaurantId(restaurant.getId())
+                .nameAddress(restaurant.getNameAddress())
+                .city(restaurant.getCity())
+                .name(restaurant.getName())
+                .licenseDate(restaurant.getLicenseDate())
+                .businessStatus(restaurant.getBusinessStatus())
+                .type(restaurant.getType())
+                .streetAddress(restaurant.getStreetAddress())
+                .lotNumberAddress(restaurant.getLotNumberAddress())
+                .postalCode(restaurant.getPostalCode())
+                .longitude(restaurant.getLongitude())
+                .latitude(restaurant.getLatitude())
+                .rating(restaurant.getRating())
                 .reviewList(reviewList)
                 .build();
     }
