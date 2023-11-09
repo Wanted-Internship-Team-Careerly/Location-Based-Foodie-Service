1aed97af52d294d0fb6dc3404e3d51f175326ffc
jonghun <jonghuncu@gmail.com>
2023-11-09 18:23:31 +0900

    feat : queryDsl 가게 리뷰

--- src/main/java/com/locationbasedfoodieservice/review/repository/ReviewRepositoryCustom.java
+++ src/main/java/com/locationbasedfoodieservice/review/repository/ReviewRepositoryCustom.java
@@ -1,7 +1,12 @@
 package com.locationbasedfoodieservice.review.repository;
 
+import com.locationbasedfoodieservice.review.entity.Review;
+import java.util.List;
+
 public interface ReviewRepositoryCustom {
 
     Integer findCountByRestaurantId(Long restaurantId);
+
+    List<Review> findReviewsByRestaurantId(Long restaurantId);
 
 }
