a5415b8c7bae01d3068c88f76ca80d2d39686ebd
jonghun <jonghuncu@gmail.com>
2023-11-10 08:34:28 +0900

    feat : loginMember 추가

--- src/main/java/com/locationbasedfoodieservice/restaurant/controller/RestaurantController.java
+++ src/main/java/com/locationbasedfoodieservice/restaurant/controller/RestaurantController.java
@@ -1,12 +1,14 @@
 package com.locationbasedfoodieservice.restaurant.controller;
 
+import com.locationbasedfoodieservice.common.config.auth.LoginMember;
 import com.locationbasedfoodieservice.restaurant.dto.response.RestaurantResponseDto;
 import com.locationbasedfoodieservice.restaurant.service.RestaurantService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,7 +19,8 @@ public class RestaurantController {
     private final RestaurantService restaurantService;
 
     @GetMapping(value = "/{restaurantId}")
-    public ResponseEntity<RestaurantResponseDto> getRestaurant(@RequestParam("restaurantId") Long restaurantId) {
+    public ResponseEntity<RestaurantResponseDto> getRestaurant(@PathVariable("restaurantId") Long restaurantId,
+            @AuthenticationPrincipal LoginMember loginMember) {
 
         return ResponseEntity.ok().body(restaurantService.getRestaurant(restaurantId));
     }
