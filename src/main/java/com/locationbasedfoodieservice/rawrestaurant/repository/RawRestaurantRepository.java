6c0a6bd722f25f720a2747da84bd90f2c59d50e6
suhwan <sueh9898@icloud.com>
2023-11-19 14:04:24 +0900

    [refactor] raw restaurant 데이터 갱신 속도 개선 (#37)
    * refactor: raw restaurant 데이터 갱신 속도 개선
    
    * chore: 변수명 수정

--- src/main/java/com/locationbasedfoodieservice/rawrestaurant/repository/RawRestaurantRepository.java
+++ src/main/java/com/locationbasedfoodieservice/rawrestaurant/repository/RawRestaurantRepository.java
@@ -1,12 +1,10 @@
 package com.locationbasedfoodieservice.rawrestaurant.repository;
 
-import com.locationbasedfoodieservice.rawrestaurant.entity.RawRestaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import com.locationbasedfoodieservice.rawrestaurant.entity.RawRestaurant;
 
 @Repository
 public interface RawRestaurantRepository extends JpaRepository<RawRestaurant, Long> {
-	Optional<RawRestaurant> findByBizplcNmAndRefineZipCd(String name, String zip);
 }
